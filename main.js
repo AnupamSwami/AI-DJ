c53aaf42a2f4283761df0e66b7d465ffb7daaeb8
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-12-19 17:46:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup() {
+    canvas = createCanvas(400,300) ;
+    canvas.position(500,200) ;
+    video = createCapture(VIDEO) ;
+    video.size(400,300) ;
+    video.position(500,200) ;
+}
+
+function draw() {
+    background('white') ;
+}
